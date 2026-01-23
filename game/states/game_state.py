75f811e982e10b7f43087c3db8a99bcee23b2eaf
saybe <vanuya.krak@gmail.com>
2026-01-23 14:16:05 +0300

    Fix movement speed scaling

--- game/states/game_state.py
+++ game/states/game_state.py
@@ -1,6 +1,15 @@
 import arcade
 
-from game.config import SCREEN_HEIGHT, SCREEN_WIDTH
+from game.config import (
+    GRAVITY,
+    PLAYER_JUMP_SPEED,
+    PLAYER_MOVE_SPEED,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+)
+from game.entities.coin import Coin
+from game.entities.hazard import Hazard
+from game.entities.platform import Platform
 from game.entities.player import Player
 from game.states.base import BaseView
 from game.systems.camera import CameraManager
@@ -30,6 +39,10 @@ class GameView(BaseView):
 
         self.score = 0
         self.time_elapsed = 0.0
+        self.level_end_x = 0
+        self._move_left = False
+        self._move_right = False
+        self._jump_requested = False
 
     def setup(self):
         self.player_list.clear()
@@ -41,6 +54,60 @@ class GameView(BaseView):
 
         self.player = Player()
         self.player_list.append(self.player)
+
+        self.score = 0
+        self.time_elapsed = 0.0
+        self.level_end_x = 0
+        self._move_left = False
+        self._move_right = False
+        self._jump_requested = False
+
+        if self.level_id == 1:
+            self._setup_level_one()
+        else:
+            self.physics_engine = None
+
+    def _setup_level_one(self):
+        self.player.center_x = 100
+        self.player.center_y = 150
+
+        ground = Platform(2400, 40)
+        ground.center_x = 1200
+        ground.center_y = 20
+        self.platform_list.append(ground)
+
+        platform_positions = [
+            (300, 140),
+            (520, 220),
+            (760, 300),
+            (980, 220),
+            (1200, 160),
+            (1450, 260),
+            (1700, 200),
+        ]
+
+        for x, y in platform_positions:
+            platform = Platform(120, 24)
+            platform.center_x = x
+            platform.center_y = y
+            self.platform_list.append(platform)
+
+        for x, y in [(300, 180), (520, 260), (760, 340), (980, 260), (1450, 300)]:
+            coin = Coin()
+            coin.center_x = x
+            coin.center_y = y
+            self.coin_list.append(coin)
+
+        for x, y in [(620, 60), (1320, 60), (1900, 60)]:
+            spikes = Hazard(width=32, height=32, damage=10, color=arcade.color.RED)
+            spikes.center_x = x
+            spikes.center_y = y
+            self.hazard_list.append(spikes)
+
+        self.level_end_x = 2100
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player, self.platform_list, gravity_constant=GRAVITY
+        )
 
     def on_show_view(self):
         super().on_show_view()
@@ -62,12 +129,55 @@ class GameView(BaseView):
 
     def on_update(self, delta_time: float):
         self.time_elapsed += delta_time
-        self.player_list.update()
         self.enemy_list.update()
         if self.physics_engine:
+            direction = (-1 if self._move_left else 0) + (1 if self._move_right else 0)
+            self.player.change_x = direction * PLAYER_MOVE_SPEED * delta_time
+            if self._jump_requested and self.physics_engine.can_jump():
+                self.player.change_y = PLAYER_JUMP_SPEED * delta_time
+                self._jump_requested = False
+            if hasattr(self.physics_engine, "gravity_constant"):
+                self.physics_engine.gravity_constant = GRAVITY * delta_time
             self.physics_engine.update()
+        else:
+            self.player_list.update()
+
+        coins_hit = arcade.check_for_collision_with_list(self.player, self.coin_list)
+        for coin in coins_hit:
+            coin.remove_from_sprite_lists()
+            self.score += getattr(coin, "value", 0)
+
+        if arcade.check_for_collision_with_list(self.player, self.hazard_list):
+            self.state_manager.set_last_score(self.score)
+            self.state_manager.show_game_over(False)
+            return
+
+        if self.level_end_x and self.player.center_x >= self.level_end_x:
+            self.state_manager.set_last_score(self.score)
+            self.state_manager.show_game_over(True)
+            return
+
+        if self.player.center_y < -200:
+            self.state_manager.set_last_score(self.score)
+            self.state_manager.show_game_over(False)
+            return
+
         self.camera.update(self.player)
 
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.P:
+        if key in (arcade.key.LEFT, arcade.key.A):
+            self._move_left = True
+        elif key in (arcade.key.RIGHT, arcade.key.D):
+            self._move_right = True
+        elif key == arcade.key.SPACE:
+            self._jump_requested = True
+        elif key == arcade.key.P:
             self.state_manager.show_pause()
+        elif key == arcade.key.ESCAPE:
+            self.state_manager.show_menu()
+
+    def on_key_release(self, key, modifiers):
+        if key in (arcade.key.LEFT, arcade.key.A):
+            self._move_left = False
+        elif key in (arcade.key.RIGHT, arcade.key.D):
+            self._move_right = False
